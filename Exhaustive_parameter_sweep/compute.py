7ae766812c8019c8d86941cabef1b3388150ffdf
Jianqing <2024023604@m.scnu.edu.cn>
2025-05-15 18:37:01 +0800

    25/05/15: ESP-初始化已完成

--- Exhaustive_parameter_sweep/compute.py
+++ Exhaustive_parameter_sweep/compute.py
@@ -1,4 +1,5 @@
 import sqlite3
+from tqdm import tqdm
 from MetaSet import advancedStructure as ad
 from .dataManager import *
 
@@ -6,36 +7,34 @@ def Comput(ids):
     DB_PATH = 'D:/WORK/Achromatic_metalens_design_in_Windows/data/Main.db'
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    
-    #  初始化，建立计算对象，设置材料
-    meta=ad.MetaEngine()
-    meta.materialSet() # 暂且如此
-    
-    for key, values in ids.items():
-        strClass,baseValue=key[0],key[1]
+
+    meta = ad.MetaEngine()
+    meta.materialSet()
+    counter = 0
+
+    for key, values in tqdm(ids.items(), desc="Base structures", unit="group"):
+        strClass, baseValue = key[0], key[1]
+
         cursor.execute("""
         SELECT parameterA, parameterB FROM BaseParameter
         WHERE baseValue = ?
         """, (baseValue,))
         row = cursor.fetchone()
-        
         p, h = row[0], row[1]
-        #   结构初始化
+
         meta.baseBuild(p)
-        for id in values:
-            #   分析输入，读取参数
-            query = f"SELECT * FROM Parameter WHERE ID = ?"
-            cursor.execute(query, (id,))
+
+        for id in tqdm(values, desc=f"→ base={baseValue}", unit="sim", leave=False):
+            #tic = time()
+            counter += 1
+
+            cursor.execute("SELECT * FROM Parameter WHERE ID = ?", (id,))
             row = cursor.fetchone()
-            parameter=[row[3],row[4],row[5]]
-            
-            #   传入参数，建立结构
+            parameter = [row[3], row[4], row[5]]
+
             meta.structureBuild(strClass, parameter, h)
-            #   进行运算，提取数据
             meta.dataAcquisition()
-            #   存入数据
             dataInput(meta, id, conn, cursor)
-            #   半重置结构
             meta.semi_Reset()
-        #   完全重置结构并Layout
+
         meta.Reset()
--- test_for_EPS.py
+++ test_for_EPS.py
@@ -1,5 +1,9 @@
 from Exhaustive_parameter_sweep import main
+from time import time
 
 if __name__ == "__main__":
+    t1=time()
     main.main_for_EPS_Initialization()
+    t2=time()
+    print(f"{t2-t1}")
     #main.main_for_EPS_Resume()
